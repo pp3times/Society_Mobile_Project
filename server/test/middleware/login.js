1362e1cd21df3a0e9c0ec7a6cd1a7604d087a343
pp3times <than2314@gmail.com>
2022-11-16 04:08:53 +0700

    Delete server/test directory

--- server/test/middleware/login.js
+++
@@ -1,8 +0,0 @@
-export const loginMiddleware = (req, res, next) => {
-	if(req.body.username === "jeremy" && 
-		 req.body.password === "3times") {
-			res.send('Login Success')
-		  next()
-		}
-	else res.send("Wrong username and password") 
-}
--- server/test/services/auth.services.js
+++
@@ -1,6 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-require('dotenv').config();
-const bcrypt = require('bcryptjs');
-const jwt = require('../utils/jwt');
