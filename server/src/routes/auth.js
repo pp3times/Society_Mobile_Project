1adcea5d651b23d0de554b976b11f1c083fc0341
Thanawat.J | Jeremy <thanawat.j2314@gmail.com>
2022-11-27 23:36:11 +0700

    feat: 🎸 bar service and new schema

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,9 +1,13 @@
-// const router = require("express").Router();
 import express from "express";
 import {
   registerControl,
   loginControl,
 } from "../controllers/auth.controller";
+import {
+	registerBarControl,
+	loginBarControl
+} from "../controllers/bar.controller"
+
 import auth from "../middlewares/auth";
 const router = express.Router();
 
@@ -12,5 +16,11 @@ router.post("/register", registerControl);
 
 // login
 router.post("/login", loginControl);
+
+// Bar Register
+router.post("/barreg", registerBarControl);
+
+// Bar Login
+router.post("/barlog", loginBarControl)
 
 export default router;
