a3f11dedb217e69cc149d3914af69b8938ffbf6f
Thanawat.J | Jeremy <thanawat.j2314@gmail.com>
2022-11-27 19:17:38 +0700

    feat: 🎸 add middleware

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getUsersControl } from "../controllers/user.controller";
+import auth from "../middlewares/auth";
 
 const router = express.Router();
 
-router.get("/get", getUsersControl);
+router.get("/get", auth, getUsersControl);
 
 export default router;
