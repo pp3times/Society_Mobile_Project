088ca310b6e85f8ee6d26ce35d8df55c83d20abb
Jeremy | Thanawat <thanawat.j2314@gmail.com>
2022-12-04 12:01:17 +0700

    :art: update layout and function

--- client/screens/user/HomeScreen.js
+++ client/screens/user/HomeScreen.js
@@ -1,16 +1,16 @@
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import React from "react";
-import { Header, BarCards } from "@/components/";
-import UserLayout from "../../components/UserLayout";
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native'
+import React from 'react'
+import {Header, BarCards} from '@/components/'
+import UserLayout from '../../components/UserLayout'
 
 const HomeScreen = () => {
   return (
     <UserLayout>
-      <View style={{ backgroundColor: "#fff", flex: 1, marginBottom:10 }}>
+      <View style={{backgroundColor: '#fff', flex: 1}}>
         <BarCards />
       </View>
     </UserLayout>
-  );
-};
+  )
+}
 
-export default HomeScreen;
+export default HomeScreen
